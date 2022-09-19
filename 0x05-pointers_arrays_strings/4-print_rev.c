11c7301cced3ea2bc75ae16d722c181d953500a6
Asante Kelvin <kelvinowusu523@gmail.com>
2022-09-19 19:05:04 +0000

    Arrays and pointer

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * print_rev - prints a string, in reverse, followed by a new line.
- *   * @s: input string.
- *    *
- *     * Return: no return.
- *      */
+ * print_rev - prints a string, in reverse, followed by a new line*
+ * @s: input string.
+ *
+ * Return: no return.
+ */
 void print_rev(char *s)
 {
 	int count = 0;
