2a618bf20b35373f7df61c1b3737f0a995d7c9c0
Asante Kelvin <kelvinowusu523@gmail.com>
2022-10-06 18:17:02 -0700

    C pointers and malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,51 @@
-#include <stdio.h>
 #include "main.h"
-/**
- * string_nconcat - concatenates two strings into one
- * @s1: first string
- * @s2: second string
- * @n: amount of bytes
- *
- * Return: a pointer to the newly allocatedspace.
- */
+#include <stdlib.h>
 
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1 : first string
+ * @s2 : second string
+ * @n : number of bytes to take from s2
+ * Return: pointer to allocated memory contain
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptcon;
-	unsigned int c1, c2, cfinal, i;
+	unsigned int s1Count, s2Count, totalCount, i;
+	char *ptr;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
-
+	}
 	if (s2 == NULL)
-		s3 = "";
-
-	for (c1 = 0; s1[c1] != '\0'; c1++)
+	{
+		s2 = "";
+	}
+	for (s1Count = 0; s1[s1Count] != '\0'; s1Count++)
+	{
 		;
-
-	for (c2 = 0; s2[c2] != '\0'; c2++)
+	}
+	for (s2Count = 0; s2[s2Count] != '\0'; s2Count++)
+	{
 		;
-
-	if (n > c2)
-		n = c2;
-
-	cfinal = c1 + n;
-
-	ptcon = malloc(cfinal + 1);
-
-	if (cfinal == NULL)
+	}
+	if (n >= s2Count)
+	{
+		n = s2Count;
+	}
+	totalCount = s1Count + n;
+	ptr = malloc(totalCount + 1);
+	if (ptr == NULL)
+	{
 		return (NULL);
-
-	for (i = 0; i < cfinal; i++)
-		if (i < c1)
-			cfinal[i] = s1[i];
+	}
+	for (i = 0; i < totalCount; i++)
+	{
+		if (i < s1Count)
+			ptr[i] = s1[i];
 		else
-			cfinal[i] = s2[i - c1];
-
-	cfinal[i] = '\0';
-
-	return (cfinal);
+			ptr[i] = s2[i - s1Count];
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
-
