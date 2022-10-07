28ea8f000b42a9fd4e82b2f72714617f403b7bfa
Asante Kelvin <kelvinowusu523@gmail.com>
2022-10-06 17:58:06 -0700

    pointers and malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,15 +15,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
+	}
 
 	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
-
+	}
 	for (i = 0; i < (nmemb * size); i++)
+	{
 		ptr[i] = 0;
+	}
 
 	return (ptr);
 }
