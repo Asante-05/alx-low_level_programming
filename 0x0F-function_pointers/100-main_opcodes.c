ab2445f8843f4781879e9a096c4ef963a219fade
Asante Kelvin <kelvinowusu523@gmail.com>
2022-10-12 21:20:20 +0000

    More C pointers and functions

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - 
+ * main - prints the opcode of the main function
  * @argc: argument count.
  * @argv: argument vector.
  *
@@ -10,8 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	char *opc = (char *) main;
-	int i, nbytes;
+	char *op = (char *) main;
+	int i, n;
 
 	if (argc != 2)
 	{
@@ -19,18 +19,18 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	nbytes = atoi(argv[1]);
+	n = atoi(argv[1]);
 
-	if (nbytes < 0)
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	for (i = 0; i < nbytes; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%02x", opc[i] & 0xFF);
-		if (i != nbytes - 1)
+		printf("%02x", op[i] & 0xFF);
+		if (i != n - 1)
 			printf(" ");
 	}
 
