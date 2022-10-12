86720b76214b4eb6c3087c78d542663f204c3936
Asante Kelvin <kelvinowusu523@gmail.com>
2022-10-12 21:06:14 +0000

    More C pointers and functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
  * print_name - passes a name to a print function
  * @name: pointer paramemter of function
  * @f: pointer to function parameter
+ *
  * Return: void (nothing)
  */
 void print_name(char *name, void (*f)(char *))
