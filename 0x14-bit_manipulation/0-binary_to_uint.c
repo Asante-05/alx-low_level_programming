779f1370dba22b39c18fb942735063dc5e87e2a0
Asante Kelvin <kelvinowusu523@gmail.com>
2022-10-28 00:46:55 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * binary_to_uint - function to convert binary to an unsigned int
+ * @b: pointer to string
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+	int i;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '1')
+		{
+			value = (value << 1) | 1;
+		}
+		else if (b[i] == '0')
+		{
+			value = value << 1;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (value);
+}
