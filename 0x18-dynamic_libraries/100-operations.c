688e76c12ca32582efb1d244887cac8d8d777536
Asante Kelvin <kelvinowusu523@gmail.com>
2022-12-12 19:27:43 +0000

    Ex3

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,35 @@
+int add(int a, int b)
+{
+	int c  = 0;
+	c = a + b;
+	return (c);
+}
+
+int sub(int a, int b)
+{
+	int c = 0;
+	c = a - b;
+	return (c);
+
+}
+
+int mul(int a, int b)
+{
+	int c = 0;
+	c = a * b;
+	return (c);
+}
+
+int div(int a, int b)
+{
+	int c = 0;
+	c = a / b;
+	return (c);
+}
+
+int mod(int a, int b)
+{
+	int c = 0;
+	c = a % b;
+	return (c);
+}
