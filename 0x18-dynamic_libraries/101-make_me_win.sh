aad75199a8b2dc621405e2dd53dd992fc24abfa6
Asante Kelvin <kelvinowusu523@gmail.com>
2022-12-12 23:30:47 +0000

    last

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
-wget -P /tmp https://github.com/Kelvin-523/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp https://github.com/Kelvin-523/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwin.so
