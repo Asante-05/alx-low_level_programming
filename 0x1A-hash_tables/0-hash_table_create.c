77126be10298929c9e424d3fc6a22a8fcd6c58b8
Asante Kelvin <kelvinowusu523@gmail.com>
2023-01-06 14:23:17 +0000

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,4 @@
-#include "header.h"
+#include "hash_tables.h"
 /**
  * hash_table_create - function to create a hash table of size (size)
  * @size: defines the size of the table
@@ -6,11 +6,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t);
+	unsigned int i = 0;
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
+	if (table == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		return (NULL);
+	}
 	table->size = size;
 
-	for (int i = 0; i < table->size; i++)
+	table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (table->array == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		return (NULL);
+	}
+
+	for (; i < size; i++)
 	{
 		table->array[i] = NULL;
 	}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -32,4 +34,5 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+hash_table_t *hash_table_create(unsigned long int size);
 #endif
